809a544a1ccb75b6b5a375bbc74bea6a7561ddcf
JimCode90 <jimmypaytan10@gmail.com>
2023-04-14 17:53:25 -0500

    video 128

---
+++ src/modules/pokemon/pages/PokemonPage.vue
@@ -0,0 +1,28 @@
+<template>
+  <h1>
+    Pokemon <span># {{ id }}</span>
+  </h1>
+</template>
+
+<script>
+export default {
+  props: {
+    id: {
+      type: Number,
+      required: true
+    }
+  },
+  data() {
+    return {
+      // id: this.$route.params.id
+    }
+  },
+  created() {
+    // const {id} = this.$route.params
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
