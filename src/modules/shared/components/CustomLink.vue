548654ec4814aa41a970d589d4dbd386f3c17154
JimCode90 <jimmypaytan10@gmail.com>
2023-04-18 17:34:16 -0500

    modulo finalizado de Vue Router y ciclo de vida de los componentes

--- src/modules/shared/components/CustomLink.vue
+++ src/modules/shared/components/CustomLink.vue
@@ -6,10 +6,11 @@
   >{{ link.name }}</a>
   <router-link
       v-else
-      :to="link.to"
-      v-slot="{ href, isActive }"
+      :to="route"
+      v-slot="{ isActive }"
   >
-    <a :href="href" :class=" isActive ? 'is-active' : 'normal-link'">{{ link.name }}</a>
+    <!--    v-slot="{ href, isActive }"   -->
+    <a :class=" isActive ? 'is-active' : 'normal-link'">{{ link.name }}</a>
   </router-link>
 
 </template>
@@ -25,6 +26,11 @@ export default {
   computed: {
     isExternalLink() {
       return this.link.to.startsWith('http')
+    },
+    route() {
+      return this.link.id === undefined
+          ? {name: this.link.to}
+          : {name: this.link.to, params: {id: this.link.id}}
     }
   }
 }
--- src/router/router.js
+++ src/router/router.js
@@ -1,29 +1,63 @@
 import {createRouter, createWebHashHistory} from "vue-router";
+import isAuthenticatedGuard from "./auth-guard";
 
 
 const routes = [
     {
         path: '/',
-        redirect: '/home'
+        redirect: '/pokemon'
     },
     {
-        path: '/home',
-        name: 'home',
-        component: () => import( /* webpackChunkName: "ListPage" */ '../modules/pokemon/pages/ListPage')
+        path: '/pokemon',
+        name: 'pokemon',
+        component: () => import( /* webpackChunkName: "PokemonLayout" */ '../modules/pokemon/layouts/PokemonLayout'),
+        children: [
+            {
+                path: ' ',
+                name: 'pokemon-home',
+                component: () => import( /* webpackChunkName: "ListPage" */ '../modules/pokemon/pages/ListPage')
+            },
+            {
+                path: 'about',
+                name: 'pokemon-about',
+                component: () => import( /* webpackChunkName: "AboutPage" */ '../modules/pokemon/pages/AboutPage')
+            },
+            {
+                path: 'pokemon-id/:id',
+                name: 'pokemon-id',
+                component: () => import( /* webpackChunkName: "PokemonPage" */ '../modules/pokemon/pages/PokemonPage'),
+                props: (route) => {
+                    const id = Number(route.params.id)
+                    return isNaN(id) ? {id: 1} : {id}
+                }
+            },
+            {
+                path: '',
+                redirect: '/pokemon/home'
+            }
+        ]
     },
     {
-        path: '/about',
-        name: 'about',
-        component: () => import( /* webpackChunkName: "AboutPage" */ '../modules/pokemon/pages/AboutPage')
-    },
-    {
-        path: '/pokemon-id/:id',
-        name: 'pokemon-id',
-        component: () => import( /* webpackChunkName: "PokemonPage" */ '../modules/pokemon/pages/PokemonPage'),
-        props: ( route ) => {
-            const  id  = Number(route.params.id)
-            return isNaN(id) ? { id: 1} : {id}
-        }
+        path: '/dbz',
+        name: 'dbz',
+        beforeEnter: [ isAuthenticatedGuard ],
+        component: () => import( /* webpackChunkName: "DragonBallLayout" */ '../modules/dbz/layouts/DragonBallLayout'),
+        children: [
+            {
+                path: 'characters',
+                name: 'dbz-characters',
+                component: () => import( /* webpackChunkName: "Characters" */ '../modules/dbz/pages/Characters')
+            },
+            {
+                path: 'about',
+                name: 'dbz-about',
+                component: () => import( /* webpackChunkName: "About" */ '../modules/dbz/pages/About')
+            },
+            {
+                path: '',
+                redirect: { name: 'dbz-characters' }
+            }
+        ]
     },
     {
         path: '/:pathMatch(.*)*',
@@ -35,5 +69,40 @@ const router = createRouter({
     history: createWebHashHistory(),
     routes,
 })
+
+// Guard Global - Sincrono
+
+// router.beforeEach((to, from, next) => {
+//     console.log({ to, from, next})
+//     const random  = Math.random() * 100
+//     if (random > 50) {
+//         console.log('autenticado')
+//         next();
+//     }else {
+//         console.log(random, 'bloqueado por el beforeEach Guard')
+//         next({ name: 'pokemon-home' })
+//     }
+// })
+
+// Guard - Asincrono
+// const canAccess = () => {
+//     return new Promise( resolve => {
+//         const random = Math.random() * 100
+//         if (random > 50) {
+//             console.log('autenticado - canAccess')
+//             resolve(true);
+//         } else {
+//             console.log(random, 'bloqueado por el beforeEach Guard - canAccess')
+//             resolve(false);
+//         }
+//     })
+// }
+//
+// router.beforeEach(async (to, from, next) => {
+//     const authorized = await canAccess()
+//     authorized
+//         ? next()
+//         : next({ name: 'pokemon-home' })
+// })
 
 export default router;
